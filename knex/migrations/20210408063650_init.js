5c5ddb1b3473a95acfd65e6bb3a68c6ba791db0f
shivamrai83 <shivamrai83@gmail.com>
2021-04-12 11:01:51 +0530

    DB comments added

--- knex/migrations/20210408063650_init.js
+++ knex/migrations/20210408063650_init.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('knex-table',table=>{
-      table.increments('id');
-      table.string('name').notNullable().unique();
-      table.string('email').notNullable().unique();
+  return knex.schema.createTable('knex-table',table=>{  // creating a table with createTable of name knex-table with specific colums.
+      table.increments('id');                           // column of id with auto increment.
+      table.string('name').notNullable().unique();      // column of name with type string, not Null & Unique.
+      table.string('email').notNullable().unique();     // column of email with type string, not Null & Unique.
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('knex_table');
+  return knex.schema.dropTable('knex_table');           // for dropping a table we use export down.
 };
   
